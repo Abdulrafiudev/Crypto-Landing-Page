6395e16fa3935b22f169efda334b1a6253d84419
Abdulrafiu Ibrahim <introvertedscholar@gmail.com>
2025-04-02 17:48:39 +0100

    fixed feature 1 bug

--- app/components/feature1/feature1.tsx
+++ app/components/feature1/feature1.tsx
@@ -15,7 +15,7 @@ const Feature1 = () => {
           <FeatureComp iphone15={iphone15top} />
           <FeatureComp2
             list={featureList1}
-            className="flex-1 flex pl-[50px] pr-[50px] gap-[80px] py-[50px] bg-[#0D0D0D] border-[1px] border-solid  rounded-[30px] border-[#2171C04D] flex-wrap max-[73.75em]:pr-[20px] max-[73.75em]:pl-[20px] max-[73.75em]:gap-[20px] "
+            className="flex-1 flex pl-[50px] pr-[50px] gap-[80px] py-[50px] bg-[#0D0D0D] border-[1px] border-solid  rounded-[30px] border-[#2171C04D] flex-wrap max-[80em]:pr-[20px] max-[80em]:pl-[30px] max-[80em]:gap-[30px] max-[80em]:py-[35px]"
             padding="pl-[130px] max-[73.75em]:pl-0"
             heading="Manage, Send and Grow"
             description="All user friendly features are are easily accessible to offer
